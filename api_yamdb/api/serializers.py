ea1598515c37f5d882f864ad3e0bedfe2d7acb80
Piotr Shopin <mail4dev2022@gmail.com>
2023-01-10 10:23:05 +0400

    dev1+dev2, модель User в отдельном приложении

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,10 @@
+import re
+
 from rest_framework import serializers
 
 from reviews.models import Category, Genre, Title
+
+from users.models import User
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -60,3 +64,56 @@ class TitleSerializer(serializers.ModelSerializer):
         """Определяет какой сериализатор будет использоваться для чтения."""
         serializer = TitleGETSerializer(title)
         return serializer.data
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',
+                  'email',
+                  'first_name',
+                  'last_name',
+                  'bio',
+                  'role')
+
+
+class UserRegSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150, required=True)
+    email = serializers.EmailField(max_length=254, required=True)
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Недопустимое имя пользователя')
+        if not re.fullmatch(r'^[\w.@+-]+', value):
+            raise serializers.ValidationError('Некорректное значения поля')
+        return value
+
+    def validate(self, data):
+        """Если юзернейм занят, а пользователя
+         с такой почтой -  нет - ошибка.
+         Если юзернейм занят, и пользователь с
+         такими данными есть - разрешить высылку повторного кода
+         """
+        email_taken = User.objects.filter(
+            email=data.get('email')).exists()
+        username_taken = User.objects.filter(
+            username=data.get('username')).exists()
+        user_exists = User.objects.filter(
+            email=data.get('email'),
+            username=data.get('username')).exists()
+
+        if (email_taken or username_taken) and not user_exists:
+            raise serializers.ValidationError(
+                'Запрос содержит email или username'
+                'зарегистрированного пользователя,'
+                'либо данные принадлежат разным пользователям'
+            )
+        return data
+
+
+class UserTokenSerializer(serializers.ModelSerializer):
+    confirmation_code = serializers.CharField(max_length=50, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'confirmation_code')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,26 @@
-from rest_framework import viewsets
-
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, permissions, status, viewsets
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
+from users.models import User
 from .serializers import (
     CategorySerializer,
     GenreSerializer,
     TitleGETSerializer,
     TitleSerializer,
+    UserRegSerializer, 
+    UserSerializer,
+    UserTokenSerializer
 )
 from .mixins import CreateListDestroyViewSet
 from reviews.models import Category, Genre, Title
-from .permissions import (AnonimReadOnly)
+from .permissions import (AnonimReadOnly, IsAdminPermission)
 
 
 class CategoryViewSet(CreateListDestroyViewSet):
@@ -38,3 +50,80 @@ class TitleViewSet(viewsets.ModelViewSet):
         if self.request.method == 'GET':
             return TitleGETSerializer
         return TitleSerializer
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminPermission]
+    lookup_field = 'username'
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('username',)
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    @action(
+        detail=False,
+        methods=['GET', 'PATCH'],
+        permission_classes=[IsAuthenticated]
+    )
+    def me(self, request):
+        user = request.user
+        if request.method == 'GET':
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if request.method == "PATCH":
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save(role=user.role, partial=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def registration(request):
+    """Создание пользователя в БД на основе полученных данных и
+    отправка кода подтверждения на почту. """
+    serializer = UserRegSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.data.get('username')
+    email = serializer.data.get('email')
+    user, _ = User.objects.get_or_create(email=email, username=username)
+    confirmation_code = default_token_generator.make_token(user)
+
+    mail = (
+        'Подтверждение регистрации',
+        f'Ваше имя пользователя: {user.username} \n'
+        f'Ваш код подтверждения: {confirmation_code}',
+        'from@example.com',
+        [user.email, ]
+    )
+    send_mail(*mail, fail_silently=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def check_code_and_create_token(request):
+    serializer = UserTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.initial_data.get('username')
+    confirmation_code = serializer.initial_data.get('confirmation_code')
+    user = get_object_or_404(User, username=username)
+    if default_token_generator.check_token(user, confirmation_code):
+        jwt_token = AccessToken.for_user(user)
+        return Response({'token': str(jwt_token)}, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
